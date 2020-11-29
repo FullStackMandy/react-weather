b17fdb5b2b1f90bc13c35f067b36101bf3e4b673
Mandy <FullStackMandy@gmail.com>
2020-11-29 17:15:05 +0100

    done

---
+++ src/Weathercard.js
@@ -0,0 +1,14 @@
+//import React from 'react';
+import './App.css';
+
+function Weathercard() {
+  return (
+    <div className="Weather-app-container">
+      <header className="Weather-app-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default Weathercard;
---
+++ src/Weatheroverview.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Weathercard from "./Weathercard";
+
+class Weatheroverview extends Component {
+
+  return() {
+    //render() {
+    <Weathercard
+      city={'Haarlem'}
+    />
+    //}
+  }
+}
+
+export default Weatheroverview;
